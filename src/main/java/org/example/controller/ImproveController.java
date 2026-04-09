aa7627013d0296e913dd26f415def1e9f23ff842
SaifulHaqueNiloy <niloyjoy7@gmail.com>
2026-04-09 11:16:02 +0600

    fix: resolve Java compile blockers in Improve module

--- src/main/java/org/example/controller/ImproveController.java
+++ src/main/java/org/example/controller/ImproveController.java
@@ -2,6 +2,7 @@ package org.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.example.service.ImproveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
